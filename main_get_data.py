d0932eb93a0d58f1cca912296d07c9d5fc338482
Hyukjoon Kwon <hj.kwon9001@gmail.com>
2020-12-16 18:03:26 +0000

    minor update

--- main_get_data.py
+++ main_get_data.py
@@ -14,12 +14,16 @@ def random_state_string(qudit_num):
         state_string = state_string + np.random.choice(['0','1','2','+','S','N','T'])
     return state_string
 
-'''For 5 qutrit'''
-qudit_num = 5
-gate_num = 10
-meas_string = '01111'
-for run in range(100):
-    state_string = random_state_string(qudit_num)
-    filename = 'Q'+str(qudit_num)+'_G'+str(gate_num)+'_seq'+format(run, '03d')
-    gate_sequence = random_gate_sequence(qudit_num,gate_num)
-    optimize_neg(state_string,gate_sequence,meas_string,path=filename)
+def get_data(qudit_num, gate_num, meas_string,Sample_N):
+    for run in range(Sample_N):
+        state_string = random_state_string(qudit_num)
+        filename = 'Q'+str(qudit_num)+'_G'+str(gate_num)+'_seq'+format(run, '03d')
+        gate_sequence = random_gate_sequence(qudit_num,gate_num)
+        optimize_neg(state_string,gate_sequence,meas_string,path=filename)
+
+'''Sample code For 5 qutrit'''
+#qudit_num = 5
+#gate_num = 10
+#meas_string = '01111'
+#Sample_N = 200
+#get_data(qudit_num, gate_num, meas_string,Sample_N)
