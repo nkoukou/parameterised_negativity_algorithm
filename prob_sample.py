00bd74a26de59a479631b8ffd1b8b5a155a85b81
NKoukou <nk2314@imperial.ac.uk>
2021-11-17 00:16:49 +0000

    improved sampling speed, but crashes my interpreter

--- prob_sample.py
+++ prob_sample.py
@@ -1,10 +1,114 @@
+from numba import(jitclass, types)
 import numpy as np
 import numpy.random as nr
-import time
-import itertools as it
+# import time
 
-from phase_space import(PhaseSpace)
+@jitclass([
+('sample_size', types.int32),
+('meas_list', types.float32[:,:,:]),
+('N', types.int8),
+('index_list', types.int8[:,:]),
+('state_qd', types.float32[:,:]),
+('gate_qd', types.float32[:,:]),
+('meas_qd', types.float32[:,:]),
+('state_pd', types.float32[:,:]),
+('gate_pd', types.float32[:,:]),
+('meas_pd', types.float32[:,:]),
+('state_sign', types.float32[:,:]),
+('gate_sign', types.float32[:,:]),
+('meas_sign', types.float32[:,:]),
+('state_neg', types.float32[:]),
+('gate_neg', types.float32[:,:]),
+('meas_neg', types.float32[:]),
+('p_estimate', types.float32)
+])
+class Sampler(object):
+    '''
+    Implements quasi-probability Monte Carlo sampling as outlined in
+    Pashayan et al. (2015).
+    '''
+    def __init__(self, sample_size, meas_list, index_list, qd_list_states,
+                 qd_list_gates, qd_list_meas, pd_list_states, pd_list_gates,
+                 pd_list_meas, sign_list_states, sign_list_gates,
+                 sign_list_meas, neg_list_states, neg_list_gates,
+                 neg_list_meas):
+        self.sample_size = sample_size
+        self.meas_list = meas_list
+        self.N = self.meas_list.shape[0]
+        self.index_list = index_list
 
+        self.state_qd = qd_list_states
+        self.gate_qd = qd_list_gates
+        self.meas_qd = qd_list_meas
+
+        self.state_pd = pd_list_states
+        self.gate_pd = pd_list_gates
+        self.meas_pd = pd_list_meas
+
+        self.state_sign = sign_list_states
+        self.gate_sign = sign_list_gates
+        self.meas_sign = sign_list_meas
+
+        self.state_neg = neg_list_states
+        self.gate_neg = neg_list_gates
+        self.meas_neg = neg_list_meas
+
+        self.p_estimate = 0.
+
+    def sample_iter(self):
+        current_ps_point = np.zeros(self.N, dtype=np.int32)
+        p_estimate = 1.
+
+        # Input states
+        for s in range(self.N):
+            ps_point = np.arange(len(self.state_pd[s]))[np.searchsorted(
+              np.cumsum(self.state_pd[s]), np.random.random(), side="right")]
+            current_ps_point[s] = ps_point
+            p_estimate *= self.state_neg[s]*self.state_sign[s][ps_point]
+
+        # Gates
+        for g in range(self.index_list.shape[0]):
+            idx = self.index_list[g]
+
+            arr_dim = np.log2(len(self.gate_pd[g]))/2
+            pq_in = 0
+            for i in range(len(idx)):
+                pq_in += current_ps_point[idx[i]]//2 * 2**(2*(arr_dim-i)-1)
+                pq_in += current_ps_point[idx[i]]%2 * 2**(2*(arr_dim-i)-2)
+            pq_in = int(pq_in)
+
+            prob = self.gate_pd[g,pq_in:pq_in+int(2**arr_dim)]
+            sign = self.gate_sign[g,pq_in:pq_in+int(2**arr_dim)]
+            neg = self.gate_neg[g,int(pq_in//(2**arr_dim))]
+
+            ps_point = np.arange(len(prob))[np.searchsorted(
+              np.cumsum(prob), np.random.random(), side="right")]
+            prob_dim = np.log2(len(prob))/2
+            for i in range(len(idx)):
+                current_ps_point[idx[i]] = int(ps_point/4**(prob_dim-1-i))%4
+            p_estimate *= neg*sign[ps_point]
+
+
+        # Measurement
+        exp_qaoa = p_estimate
+        temp = 0
+        for m in range(self.N):
+            p_estimate *= self.meas_qd[m,current_ps_point[m]]
+
+            m_next = (m+1)%self.N
+            temp += 0.5 * self.meas_qd[m_next,current_ps_point[m_next]]*\
+                          self.meas_qd[m,current_ps_point[m]]
+        exp_qaoa *= temp
+
+        self.p_estimate = exp_qaoa # p_estimate #
+
+    def sample(self):
+        p_estimate = 0.
+        for n in range(self.sample_size):
+            if n%(self.sample_size//10)==0: print(n/self.sample_size*100, "%")
+            self.sample_iter()
+            p_estimate += 1./self.sample_size * self.p_estimate
+        self.p_estimate = p_estimate
 
 def get_qd_output(circuit, par_list, ps):
     '''
@@ -96,77 +200,42 @@ def get_qd_output(circuit, par_list, ps):
     }
     return output
 
-def sample_circuit(circuit, qd_output, sample_size=int(1e4)):
-    '''
-    The function performing the Monte Carlo sampling as outlined in
-    Pashayan et al. (2015) for the compressed version of the given circuit
-    and the parameter list. It prints the method we used, the sampling result
-    (p_estimate), and the computation time. It also returns the sampling
-    result and the full list of p_estimate of each iteration ('plot').
-    '''
-    def sample_iter():
-        current_ps_point = []
-        p_estimate = 1.
+def prepare_sampler(circuit, par_list, ps):
+    meas_list = np.stack(circuit["meas_list"]).astype(np.float32)
+    index_list = np.array(circuit["index_list"]).astype(np.int8)
 
-        # Input states - PD NOT QD
-        for s, pd_state in enumerate(qd_output['pd_list_states']):
-            prob = pd_state.flatten()
-            neg = qd_output['neg_list_states'][s]
-            sign = qd_output['sign_list_states'][s].flatten()
+    output = get_qd_output(circuit, par_list, ps)
 
-            ps_point = nr.choice(np.arange(len(prob)), p=prob)
-            current_ps_point.append(ps_point)
-            p_estimate *= neg*sign[ps_point]
+    qd_list_states = np.stack([dist.flatten().astype(np.float32)
+                               for dist in output["qd_list_states"]])
+    qd_list_gates = np.stack([dist.flatten().astype(np.float32)
+                              for dist in output["qd_list_gates"]])
+    qd_list_meas = np.stack([dist.flatten().astype(np.float32)
+                             for dist in output["qd_list_meas"]])
 
-        # Gates
-        for g, pd_gate in enumerate(qd_output['pd_list_gates']):
-            idx = circuit['index_list'][g]
+    neg_list_states = np.array(output["neg_list_states"]).astype(np.float32)
+    neg_list_gates = np.stack([dist.flatten().astype(np.float32)
+                               for dist in output["neg_list_gates"]])
+    neg_list_meas = np.array(output["neg_list_meas"]).astype(np.float32)
 
-            pq_in = [[current_ps_point[idx[i]]//2, current_ps_point[idx[i]]%2]
-                     for i in range(len(idx))]
-            pq_in = tuple([item for sublist in pq_in for item in sublist])
+    pd_list_states = np.stack([dist.flatten().astype(np.float32)
+                               for dist in output["pd_list_states"]])
+    pd_list_gates = np.stack([dist.flatten().astype(np.float32)
+                              for dist in output["pd_list_gates"]])
+    pd_list_meas = np.stack([dist.flatten().astype(np.float32)
+                             for dist in output["pd_list_meas"]])
 
-            prob = pd_gate[pq_in].flatten()
-            neg = qd_output['neg_list_gates'][g][pq_in]
-            sign = qd_output['sign_list_gates'][g][pq_in].flatten()
+    sign_list_states = np.stack([dist.flatten().astype(np.float32)
+                                 for dist in output["sign_list_states"]])
+    sign_list_gates = np.stack([dist.flatten().astype(np.float32)
+                                for dist in output["sign_list_gates"]])
+    sign_list_meas = np.stack([dist.flatten().astype(np.float32)
+                               for dist in output["sign_list_meas"]])
 
-            ps_point = nr.choice(np.arange(len(prob)), p=prob)
-            str_repr = np.base_repr(ps_point, 4).zfill(len(idx))
-            for i in range(len(idx)):
-                current_ps_point[idx[i]] = int(str_repr[i])
-            p_estimate *= neg*sign[ps_point]
-
-        # Measurement - QD NOT PD
-        exp_qaoa = p_estimate
-        temp = 0
-        for m, meas in enumerate(circuit['meas_list']):
-            wf = qd_output['qd_list_meas'][m].flatten()
-            p_estimate *= wf[current_ps_point[m]]
-
-            m_next = (m+1)%len(circuit['meas_list'])
-            wf_next = qd_output['qd_list_meas'][m_next].flatten()
-            temp += 0.5 * wf_next[current_ps_point[m_next]] * \
-                          wf[current_ps_point[m]]
-
-        exp_qaoa *= temp
-
-        return exp_qaoa
-
-    start_time = time.time()
-    p_estimate = np.zeros(sample_size)
-    print("\n"+"SAMPLING")
-    for n in range(sample_size):
-        if n%(sample_size//10)==0: print(n/sample_size*100, "%")
-        p_estimate[n] = sample_iter()
-    sampling_time = time.time() - start_time
-
-    print('====================== Sampling Results ======================')
-    print('p_estimate:    ', np.average(p_estimate))
-    print('Sample size:   ', sample_size)
-    print('Sampling time: ', sampling_time)
-    print('==============================================================')
-
-    return p_estimate
+    return(meas_list, index_list, qd_list_states, qd_list_gates, qd_list_meas,
+           pd_list_states, pd_list_gates, pd_list_meas, sign_list_states,
+           sign_list_gates, sign_list_meas, neg_list_states, neg_list_gates,
+           neg_list_meas)
 
 
 
@@ -178,6 +247,79 @@ def sample_circuit(circuit, qd_output, sample_size=int(1e4)):
 
 
 
+
+
+# def sample_circuit(circuit, qd_output, sample_size=int(1e4)):
+#     '''
+#     The function performing the Monte Carlo sampling as outlined in
+#     Pashayan et al. (2015) for the compressed version of the given circuit
+#     and the parameter list. It prints the method we used, the sampling result
+#     (p_estimate), and the computation time. It also returns the sampling
+#     result and the full list of p_estimate of each iteration ('plot').
+#     '''
+#     def sample_iter():
+#         current_ps_point = []
+#         p_estimate = 1.
+
+#         # Input states - PD NOT QD
+#         for s, pd_state in enumerate(qd_output['pd_list_states']):
+#             prob = pd_state.flatten()
+#             neg = qd_output['neg_list_states'][s]
+#             sign = qd_output['sign_list_states'][s].flatten()
+
+#             ps_point = nr.choice(np.arange(len(prob)), p=prob)
+#             current_ps_point.append(ps_point)
+#             p_estimate *= neg*sign[ps_point]
+
+#         # Gates
+#         for g, pd_gate in enumerate(qd_output['pd_list_gates']):
+#             idx = circuit['index_list'][g]
+
+#             pq_in = [[current_ps_point[idx[i]]//2,current_ps_point[idx[i]]%2]
+#                      for i in range(len(idx))]
+#             pq_in = tuple([item for sublist in pq_in for item in sublist])
+
+#             prob = pd_gate[pq_in].flatten()
+#             neg = qd_output['neg_list_gates'][g][pq_in]
+#             sign = qd_output['sign_list_gates'][g][pq_in].flatten()
+
+#             ps_point = nr.choice(np.arange(len(prob)), p=prob)
+#             str_repr = np.base_repr(ps_point, 4).zfill(len(idx))
+#             for i in range(len(idx)):
+#                 current_ps_point[idx[i]] = int(str_repr[i])
+#             p_estimate *= neg*sign[ps_point]
+
+#         # Measurement - QD NOT PD
+#         exp_qaoa = p_estimate
+#         temp = 0
+#         for m, meas in enumerate(circuit['meas_list']):
+#             wf = qd_output['qd_list_meas'][m].flatten()
+#             p_estimate *= wf[current_ps_point[m]]
+
+#             m_next = (m+1)%len(circuit['meas_list'])
+#             wf_next = qd_output['qd_list_meas'][m_next].flatten()
+#             temp += 0.5 * wf_next[current_ps_point[m_next]] * \
+#                           wf[current_ps_point[m]]
+
+#         exp_qaoa *= temp
+
+#         return p_estimate # exp_qaoa
+
+#     start_time = time.time()
+#     p_estimate = np.zeros(sample_size)
+#     print("\n"+"SAMPLING")
+#     for n in range(sample_size):
+#         if n%(sample_size//10)==0: print(n/sample_size*100, "%")
+#         p_estimate[n] = sample_iter()
+#     sampling_time = time.time() - start_time
+
+#     print('====================== Sampling Results ======================')
+#     print('p_estimate:    ', np.average(p_estimate))
+#     print('Sample size:   ', sample_size)
+#     print('Sampling time: ', sampling_time)
+#     print('==============================================================')
+
+#     return p_estimate
 
 
 
--- test_qaoa.py
+++ test_qaoa.py
@@ -1,11 +1,18 @@
+import time
+from multiprocessing import Pool
+# from pathos import multiprocessing
+# import os
+# os.environ['NUMBA_DISABLE_INTEL_SVML']  = '1'
+
 import numpy as np
+import numpy.random as nr
 from scipy.linalg import(expm)
 
 from qubit_circuit_generator import(show_connectivity)
 from compression import(compress_circuit)
 from frame_opt import(init_x_list, get_negativity_circuit, sequential_para_opt)
 from phase_space import(PhaseSpace)
-from prob_sample import(get_qd_output, sample_circuit)
+from prob_sample import(prepare_sampler, Sampler)
 from qubit_frame_Wigner import(F, G, DIM, x0)
 
 ps_Wigner = PhaseSpace(F, G, x0, DIM)
@@ -48,7 +55,7 @@ def qaoa_maxcut(G, beta_list, gamma_list):
                'index_list': index_list, 'meas_list': meas_list}
     return circuit
 
-
+sample_size = int(1e8)
 x0 = ps_Wigner.x0
 W = ps_Wigner.W
 
@@ -69,15 +76,25 @@ for b in range(betas.size):
         x_circ  = init_x_list(circuit, x0)
         x_out, neg_list = sequential_para_opt(W, circuit, x_circ,
                                               l=1, niter=1)
-        output = get_qd_output(circuit, x_out, ps_Wigner)
-        exp_qaoa = sample_circuit(circuit, output, sample_size = int(1e8))
+        meas_list, index_list, qd_list_states, qd_list_gates, qd_list_meas,\
+        pd_list_states, pd_list_gates, pd_list_meas, sign_list_states,\
+        sign_list_gates, sign_list_meas, neg_list_states, neg_list_gates,\
+        neg_list_meas = prepare_sampler(circuit=circuit,
+                                        par_list=x_out, ps=ps_Wigner)
 
-        temp = np.prod(np.array(output["neg_list_states"]))
-        for neg_gate in output["neg_list_gates"]:
-            temp *= neg_gate.max()
-        temp *= np.prod(np.array(output["neg_list_meas"]))
-        neg_circ[b,g] = np.log2(temp)
-        p_est[b, g] = np.average(exp_qaoa)
+        qaoa_sampler = Sampler(sample_size, meas_list, index_list,
+          qd_list_states, qd_list_gates, qd_list_meas, pd_list_states,
+          pd_list_gates, pd_list_meas, sign_list_states, sign_list_gates,
+          sign_list_meas, neg_list_states, neg_list_gates, neg_list_meas)
+        qaoa_sampler.sample()
+
+        p_est[b, g] = qaoa_sampler.p_estimate
+
+        neg = np.prod(neg_list_states)
+        for neg_gate in neg_list_gates:
+            neg *= neg_gate.max()
+        neg *= np.prod(neg_list_meas)
+        neg_circ[b,g] = np.log2(neg)
 
         np.save("neg_circ.npy", neg_circ)
         np.save("p_est.npy", p_est)
@@ -99,34 +116,112 @@ plt.plot(neg.flatten(), (p_est - p_exact).flatten(), 'o')
 plt.xlabel("Negativity")
 plt.ylabel(r'$\hat{p} - p$')
 
-plt.figure()
-plt.plot((p_est - p_exact).flatten(), 'o')
-plt.xlabel(r'$(\beta, \gamma)$')
-plt.ylabel(r'$\hat{p} - p$')
-
 
 
 
 ### TEST SAMPLING ###
 # from qubit_circuit_components import(makeState, makeGate)
 
-# phi = 0.8 * np.pi
-# test_circuit = {'state_list': [makeState('0') for i in range(3)],
+# phi = 0.7 * np.pi
+# circuit = {'state_list': [makeState('0') for i in range(3)],
 #                 'gate_list': [U_mix(phi), makeGate('C+'), X, makeGate('H')],
 #                 'index_list': [[0], [0,1], [1], [2]],
-#                 'meas_list': [makeState('0'), makeState('1'), makeState('+')]}
+#                 'meas_list': [makeState('0'),makeState('1'),makeState('+')]}
 
-# test_circuit = compress_circuit(test_circuit, n=3)
-# x_circuit1 = init_x_list(test_circuit, x0)
-# x_out1, neg_list_seq1 = sequential_para_opt(W, test_circuit,
-#                                             x_circuit1, l=1, niter=1)
-# output = get_qd_output(test_circuit, x_out1, ps_Wigner)
-# p_est = sample_circuit(test_circuit, output,
-#                                           sample_size = int(5e5))
+# circuit = compress_circuit(circuit, n=2)
+# x_circuit = init_x_list(circuit, x0)
+# x_out, neg_list_seq = sequential_para_opt(W, circuit,
+#                                             x_circuit, l=1, niter=1)
+
+# meas_list, index_list, qd_list_states, qd_list_gates, qd_list_meas,\
+# pd_list_states, pd_list_gates, pd_list_meas, sign_list_states,\
+# sign_list_gates, sign_list_meas, neg_list_states, neg_list_gates,\
+# neg_list_meas = prepare_sampler(circuit=circuit, par_list=x_out,ps=ps_Wigner)
+
+# sample_size = int(1e8)
+# test = Sampler(sample_size, meas_list, index_list, qd_list_states,
+#                 qd_list_gates, qd_list_meas, pd_list_states, pd_list_gates,
+#                 pd_list_meas, sign_list_states, sign_list_gates,
+#                 sign_list_meas, neg_list_states, neg_list_gates,
+#                 neg_list_meas)
+
+# print(np.cos(phi)**2)
+# test.sample()
+# print(test.p_estimate)
+
+
+### TESTING
+
+# output = get_qd_output(circuit, x_out, ps_Wigner)
+
+# p_est = sample_circuit(circuit, output, sample_size=int(1e5))
 # print(np.cos(phi)**2)
 
+# prob = np.array([0.6, 0.2, 0.1, 0.1])
+# N = int(1e5)
+# ps_point1 = np.zeros(N)
+# ps_point2 = np.zeros(N)
+# for i in range(N):
+#     if i%(N//10)==0: print(i/N*100, "%")
+#     ps_point1[i] = np.arange(len(prob))[np.searchsorted(
+#               np.cumsum(prob), np.random.random(), side="right")]
+#     ps_point2[i] = nr.choice(np.arange(len(prob)), p=prob)
+
+# import matplotlib.pylab as plt
+
+# plt.hist(ps_point1)
+# plt.hist(ps_point2, rwidth=0.5)
 
 
+# i=1
+# test_pd = output["pd_list_gates"][i]
+# test_sg = output["sign_list_gates"][i]
+# test_ng = output["neg_list_gates"][i]
+
+# current_ps_point = [1,0,1,1]
+# idx = [0,1]
+# pq_in1 = [[current_ps_point[idx[i]]//2, current_ps_point[idx[i]]%2]
+#           for i in range(len(idx))]
+# pq_in1 = tuple([item for sublist in pq_in1 for item in sublist])
+# prob1 = test_pd[pq_in1].flatten()
+# sign1 = test_sg[pq_in1].flatten()
+# neg1 = test_ng[pq_in1]
+
+# arr_dim = np.log2(len(test_pd.flatten()))/2
+# pq_in2 = 0
+# for i in range(len(idx)):
+#     pq_in2 += current_ps_point[idx[i]]//2 * 2**(2*(arr_dim-i)-1)
+#     pq_in2 += current_ps_point[idx[i]]%2 * 2**(2*(arr_dim-i)-2)
+# pq_in2 = int(pq_in2)
+# prob2 = test_pd.flatten()[pq_in2:pq_in2+int(2**arr_dim)]
+# sign2 = test_sg.flatten()[pq_in2:pq_in2+int(2**arr_dim)]
+# neg2 = test_ng.flatten()[int(pq_in2//(2**arr_dim))]
+
+# print(test_pd.flatten().shape)
+# print(test_pd.shape, test_sg.shape)
+
+# print("TEST:", np.allclose(prob1, prob2))
+# print("TEST:", np.allclose(sign1, sign2))
+# print("TEST:", np.allclose(neg1, neg2))
+
+# ps_point = nr.choice(np.arange(len(prob1)), p=prob1)
+# str_repr = np.base_repr(ps_point, 4).zfill(len(idx))
+# print(ps_point, str_repr)
+# print(ps_point//4**(arr_dim-1-0))
+# print(ps_point//4**(arr_dim-1-1))
+# for i in range(len(idx)):
+#     p1 = int(ps_point//4**(np.log2(len(prob2))/2 -1-i))%4
+#     p2 = int(str_repr[i])
+#     print(p1,p2)
+
+
+
+
+# pool = multiprocessing.processing_pool()
+# def f(n):
+#     print(n/sample_size*100, "%")
+# p = Pool(8)
+# p.map(f, range(sample_size))
 
 
 
